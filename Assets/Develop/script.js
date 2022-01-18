08a323a198f18f58eb084edfaaff9adbd543f315
Stephanie Mathis <stephaniedmathis@gmail.com>
2022-01-18 16:55:44 -0500

    Update JS code

--- Assets/Develop/script.js
+++ Assets/Develop/script.js
@@ -7,18 +7,18 @@ var symbolChar = "@#$!%^&*()_+=-";
 var passwordLength = 128
 var passwordValue = '';
 
-
 function generatePassword(){
 prompt("How many characters would you like your password to contain?");
 prompt("Click OK to confirm whether or not to include lowercase characters.");
 prompt("Click OK to confirm whether or not to include uppercase characters.");
 prompt("Click OK to confirm whether or not to include numeric characters.");
 prompt("Do you want to include special characters?");
-  for (let i=0; i < passwordLength; i++) {
-    let characters = Math.floor (Math.random() * generateBtn.length);
-    passwordValue += characters.substring(passwordValue, passwordValue +1);
 
-return ("Generated password will go here!");
+  for (let i=0; i < passwordLength; i++) {
+    let passwordValue = Math.floor (Math.random() * uppercaseChar + lowercaseChar + numericalChar + symbolChar.length);
+    passwordValue += uppercaseChar,lowercaseChar,numericalChar,symbolChar.substring(passwordValue +1);
+
+    return ("Generated password will go here!");
 
 }
 
