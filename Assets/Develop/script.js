314ff3156c3ecf7e2a3a82e833d19e30a9304b35
Stephanie Mathis <stephaniedmathis@gmail.com>
2022-01-18 18:16:39 -0500

    Add JS code to repo

--- Assets/Develop/script.js
+++ Assets/Develop/script.js
@@ -1,32 +1,56 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var lowercaseChar = "abcdefghijklmnopqrstuvwxyz"
-var numericalChar = "0123456789"
-var symbolChar = "@#$!%^&*()_+=-";
-var passwordLength = 128
-var passwordValue = '';
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" //capital letter characters
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyz" //lowercase letter characters
+var numericalChar = "0123456789" // number characters
+var symbolChar = "@#$!%^&*()_+=-"; // special characters
+var finalPassword = ''; // final password from generator
+var chosenChar = ''; // User will enter chosenChar - this could be anything
 
+// Acceptance criteria and prompts
 function generatePassword(){
-prompt("How many characters would you like your password to contain?");
-prompt("Click OK to confirm whether or not to include lowercase characters.");
-prompt("Click OK to confirm whether or not to include uppercase characters.");
-prompt("Click OK to confirm whether or not to include numeric characters.");
-prompt("Do you want to include special characters?");
+var passwordLength = prompt ("How many characters would you like your password to contain?");
+var islowercaseChar = confirm ("Click OK to confirm whether or not to include lowercase characters.");
+var isuppercaseChar = confirm ("Click OK to confirm whether or not to include uppercase characters.");
+var isnumericalChar = confirm("Click OK to confirm whether or not to include numeric characters.");
+var issymbolChar = confirm ("Do you want to include special characters?");
 
-  for (let i=0; i < passwordLength; i++) {
-    let passwordValue = Math.floor (Math.random() * uppercaseChar + lowercaseChar + numericalChar + symbolChar.length);
-    passwordValue += uppercaseChar,lowercaseChar,numericalChar,symbolChar.substring(passwordValue +1);
+// Write out if "statements" for characters ALL *Char
 
-    return ("Generated password will go here!");
-
+if (islowercaseChar){
+  chosenChar += lowercaseChar;
+  console.log(chosenChar)
 }
 
+if (isuppercaseChar){
+  chosenChar += isuppercaseChar;
+  console.log(chosenChar)
+}
+
+if (isnumericalChar){
+  chosenChar += isnumericalChar;
+  console.log(chosenChar)
+}
+
+if (issymbolChar){
+  chosenChar += issymbolChar;
+  console.log(chosenChar)
+}
+
+// For loop for password generator and include floor & random
+
+  for (let i=0; i < passwordLength; i++) {
+    let randomValue = Math.floor (Math.random() * chosenChar.length);
+    finalPassword += chosenChar[randomValue];
+
+}
+return finalPassword;
 }
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  passwordText.value = password;
 
 }
 // Add event listener to generate button
