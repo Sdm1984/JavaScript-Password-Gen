70a19b819068ecb34ce2daaf4e456d811dd088b3
Stephanie Mathis <stephaniedmathis@gmail.com>
2022-01-17 23:58:51 -0500

    updated code

--- Assets/Develop/script.js
+++ Assets/Develop/script.js
@@ -4,31 +4,28 @@ var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789
 var passwordLength = 128
 var passwordValue = '';
 
+
 function generatePassword(){
-
-  for (let i=0; i < passwordLength; i++) {
-    let number = Math.floor (Math.random() * generateBtn.length);
-    passwordValue += characters.substring(number, number +1);
-
-return ("Generated password will go here!");
-
-}
-
 prompt("How many characters would you like your password to contain?");
 prompt("Click OK to confirm whether or not to include lowercase characters.");
 prompt("Click OK to confirm whether or not to include uppercase characters.");
 prompt("Click OK to confirm whether or not to include numeric characters.");
 prompt("Do you want to include special characters?");
+  for (let i=0; i < passwordLength; i++) {
+    let characters = Math.floor (Math.random() * generateBtn.length);
+    passwordValue += characters.substring(passwordValue, passwordValue +1);
 
+return ("Generated password will go here!");
 
+}
+
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-}
